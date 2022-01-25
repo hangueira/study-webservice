ffb0c1f589db7e0c59cbb0399c8eec45e3a6efc3
hangueira <hks713@naver.com>
2022-01-25 13:39:16 +0900

    웹서비스 스터디 초기세팅

---
+++ src/test/java/com/hks/studywebservice/StudyWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hks.studywebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
